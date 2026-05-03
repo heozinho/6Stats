8cc2a98d8ceabb1a9ea243dcaca1b14015c9d6ca
n1352985 <n1352985.my.ac.uk>
2026-05-03 19:30:07 +0100

    chore: add Pinggy tunnel host to allowed list in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['proud-oranges-care.loca.lt'],
+    allowedHosts: ['proud-oranges-care.loca.lt', 'rjtkz-2a00-23c7-175a-801-a074-e3b3-5d74-9d76.run.pinggy-free.link'],
   },
 })
